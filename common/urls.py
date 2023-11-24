0a40e26bfdbdada133477b55375749064d38242f
asliddin750750 <asliddin750750@gmail.com>
2023-11-24 16:34:48 +0500

    stat

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from common.views import ClassNamesView
+from common.views import ClassNamesView, StatView
 
 urlpatterns = [
-    path('class-names/', ClassNamesView.as_view(), name='class-names')
+    path('class-names/', ClassNamesView.as_view(), name='class-names'),
+    path('stat/', StatView.as_view(), name='stat')
 ]
--- common/views.py
+++ common/views.py
@@ -1,10 +1,26 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from common.models import ClassName
 from common.serializers import ClassNamesSerializer
+from user.models import User, Pupil
 
 
 class ClassNamesView(ListAPIView):
     serializer_class = ClassNamesSerializer
     queryset = ClassName.objects.all()
     pagination_class = None
+
+
+class StatView(APIView):
+    def get(self, request):
+        teachers_count = User.objects.filter(userType=User.UserTypeChoices.TEACHER).count()
+        pupils_count = Pupil.objects.count()
+        class_names_count = ClassName.objects.count()
+
+        return Response({
+            'teachers': teachers_count,
+            'pupils': pupils_count,
+            'class_names': class_names_count
+        })
