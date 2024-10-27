841c13c498e3a67278bd60f3ca0d697be104e26e
asliddin750750 <asliddin750750@gmail.com>
2024-10-28 00:49:39 +0500

    added image field to user form

--- common/admin.py
+++ common/admin.py
@@ -2,12 +2,7 @@ from django.contrib import admin
 
 from common.models import ClassName
 
+
 @admin.register(ClassName)
 class ClassNameAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
-
-    # class Meta:
-    #     model = ClassName
-
-
-# admin.site.register(ClassName)
